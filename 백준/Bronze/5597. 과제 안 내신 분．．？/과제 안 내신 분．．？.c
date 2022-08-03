5c4ba324ac9ecb64f98b186d23437aa8ad238e54
DeadBBall <89844277+DeadBBall@users.noreply.github.com>
2022-08-03 22:01:12 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.c
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    int n[30] = {0};
+    int a[31] = {0};
     for(int i = 0; i < 28; i++)
     {
-        int tmp;
-        scanf("%d", &tmp);
-        n[tmp - 1] = 1;
+        int n;
+        scanf("%d", &n);
+        a[n] = 1;
     }
-    for(int i = 0; i < 30; i++)
+    for(int i = 1; i < 31; i++)
     {
-        if(!n[i])
-            printf("%d\n", i + 1);
+        if (!a[i])
+            printf("%d\n", i);
     }
 }

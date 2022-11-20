aee4d352e27cbabac8f872518f469dcdc7fa0aa8
DeadBBall <89844277+DeadBBall@users.noreply.github.com>
2022-11-20 12:03:58 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m, arr[8], ans[8];
+
+void solve(int cnt)
+{
+	if(cnt == m)
+	{
+		for(int i = 0; i < m; i++)
+			cout << ans[i] << " ";
+		cout << "\n";
+		return ;
+	}
+	for(int i = 0; i < n; i++)
+	{
+		if(cnt == 0 || ans[cnt - 1] <= arr[i])
+		{
+			ans[cnt] = arr[i];
+			solve(cnt + 1);
+		}
+	}
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n >> m;
+	for(int i = 0; i < n; i++)
+		cin >> arr[i];
+	sort(arr, arr + n);
+	solve(0);
+}

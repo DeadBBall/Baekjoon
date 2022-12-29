b1d092ba0e7b165473afd525f8cbbb1c657c2eb2
DeadBBall <89844277+DeadBBall@users.noreply.github.com>
2022-12-29 18:35:44 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m, arr[9], ans[9];
+bool visit[9];
+
+void solve(int cnt)
+{
+	if(cnt > m)
+	{
+		for(int i = 1; i <= m; i++)
+			cout << ans[i] << " ";
+		cout << "\n";
+		return;
+	}
+	int check = 0;
+	for(int i = 1; i <= n; i++)
+	{
+		if(!visit[i] && check != arr[i])
+		{
+			visit[i] = true;
+			check = arr[i];
+			ans[cnt] = arr[i];
+			solve(cnt + 1);
+			visit[i] = false;
+		}
+	}
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n >> m;
+	for(int i = 1; i <= n; i++)
+		cin >> arr[i];
+	sort(arr + 1, arr + n + 1);
+	solve(1);
+}

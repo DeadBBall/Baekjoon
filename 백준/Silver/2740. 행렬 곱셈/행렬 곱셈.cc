6812e99ea2dd4dbd9f449a42a29dfed707a5a8c6
morenmore <89844277+DeadBBall@users.noreply.github.com>
2023-01-17 22:28:16 +0900

    [Silver V] Title: 행렬 곱셈, Time: 0 ms, Memory: 2136 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int n, m, k, matrix1[100][100], matrix2[100][100], ans[100][100];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n >> m;
+	for(int y = 0; y < n; y++)
+		for(int x = 0; x < m; x++)
+			cin >> matrix1[y][x];
+	cin >> m >> k;
+	for(int y = 0; y < m; y++)
+		for(int x = 0; x < k; x++)
+			cin >> matrix2[y][x];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			for (int k = 0; k < m; k++)
+				ans[i][j] += matrix1[i][k] * matrix2[k][j];
+			cout << ans[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}

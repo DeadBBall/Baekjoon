02675e8018d8c902d5832870159a00a96122976d
morenmore <89844277+DeadBBall@users.noreply.github.com>
2023-09-08 09:50:52 +0900

    [Gold III] Title: 폴더 정리 (small), Time: 1268 ms, Memory: 108292 KB -BaekjoonHub

--- 백준/Gold/22860. 폴더 정리 （small）/폴더 정리 （small）.java
+++ 백준/Gold/22860. 폴더 정리 （small）/폴더 정리 （small）.java
@@ -12,44 +12,31 @@ public class Main {
 	static Scanner sc = new Scanner(System.in);
 	static int n, m;
 	static Map<String, List<String>> folders;
-	static Map<String, Integer> nameToNum;
-	static Map<Integer, String> numToName;
-	static List<Integer>[] children;
+	static Map<String, List<String>> children;
 	static Set<String> files;
 	
 	static void input() {
 		n = sc.nextInt();
 		m = sc.nextInt();
-		children = new ArrayList[n + 1];
-		nameToNum = new HashMap<>();
+		children = new HashMap<>();
 		folders = new HashMap<>();
-		numToName = new HashMap<>();
-		int folderIdx = 0;
 		for (int i = 0; i < n + m; i++) {
 			String p = sc.next();
 			String f = sc.next();
 			int c = sc.nextInt();
-			if(!nameToNum.containsKey(p)) {
-				nameToNum.put(p, folderIdx);
-				numToName.put(folderIdx, p);
-				children[folderIdx++] = new ArrayList<>();
+			if(!folders.containsKey(p)) {
 				folders.put(p, new ArrayList<>());
+				children.put(p, new ArrayList<>());
 			}
-			if (c == 1) {
-				int idx = nameToNum.get(p);
-				if(!nameToNum.containsKey(f)) {
-					nameToNum.put(f, folderIdx);
-					numToName.put(folderIdx, f);
-					children[folderIdx] = new ArrayList<>();
-					folders.put(f, new ArrayList<>());					
-					children[idx].add(folderIdx++);
-				}
-				else {
-					children[idx].add(nameToNum.get(f));
-				}
-			} else {
-				List<String> tmpList = folders.get(p);
-				tmpList.add(f);
+			if(!folders.containsKey(f)) {
+				folders.put(f, new ArrayList<>());
+				children.put(f, new ArrayList<>());
+			}
+			if(c == 1) {
+				children.get(p).add(f);
+			}
+			else {
+				folders.get(p).add(f);
 			}
 		}
 	}
@@ -61,25 +48,18 @@ public class Main {
 			String target = inputStr[inputStr.length - 1];
 			int sum = 0;
 			files = new HashSet<>();
-			Queue<Integer> q = new LinkedList<>();
-			List<String> tmpList = folders.get(target);
-			for (int j = 0; j < tmpList.size(); j++) {
-				files.add(tmpList.get(j));
-				sum++;
-			}
-			for(int j = 0; j < children[nameToNum.get(target)].size(); j++) {
-				q.add(children[nameToNum.get(target)].get(j));
-			}
-			while (!q.isEmpty()) {
-				int num = q.remove();
-				String folder = numToName.get(num);
-				tmpList = folders.get(folder);
-				for(int j = 0; j < tmpList.size(); j++) {
-					files.add(tmpList.get(j));
+			Queue<String> q = new LinkedList<>();
+			q.add(target);
+			while(!q.isEmpty()) {
+				target = q.remove();
+				List<String> tmpList = folders.get(target);
+				for(int i = 0; i < tmpList.size(); i++) {
+					files.add(tmpList.get(i));
 					sum++;
 				}
-				for(int j = 0; j < children[nameToNum.get(folder)].size(); j++) {
-					q.add(children[nameToNum.get(folder)].get(j));
+				tmpList = children.get(target);
+				for(int i = 0; i < tmpList.size(); i++) {
+					q.add(tmpList.get(i));
 				}
 			}
 			System.out.println(files.size() + " " + sum);

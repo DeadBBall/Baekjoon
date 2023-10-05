2283b4cf60f200fa5036460a5790b3e45ceab6d2
morenmore <89844277+DeadBBall@users.noreply.github.com>
2023-10-05 09:05:26 +0900

    [Bronze II] Title: 수 정렬하기, Time: 280 ms, Memory: 20872 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,41 +1,30 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static int n;
+    static int[] numbers;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        input();
+        printAns();
+    }
 
-		Scanner sc = new Scanner(System.in);
-		
-		//입력받을 수의 개수
-		int n = sc.nextInt();
-		
-		//입력받는 수를 요소로 갖는 배열
-		int[] arr = new int[n];
-		
-		//수를 입력받아 배열의 요소로 대입
-		for(int i=0; i<n; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		//삽입 정렬
-		for(int i=1; i<n; i++) {
-			for(int j=1; j<=i ;j++) {
-				if(arr[i-j+1]<arr[i-j]) {
-					int num = arr[i-j+1];
-					arr[i-j+1] = arr[i-j];
-					arr[i-j] = num;
-				}else {
-					break;
-				}
-			}
-		}
-		
-		
-		//배열 전체 출력
-		for(int k = 0;  k<n; k++) {
-			System.out.println(arr[k]);
-		}
-		
-		sc.close();
-	}
+    static void input() {
+        n = sc.nextInt();
+        numbers = new int[n];
+        for(int idx = 0; idx < n; idx++) {
+            numbers[idx] = sc.nextInt();
+        }
+
+        Arrays.sort(numbers);
+    }
+
+    static void printAns() {
+        StringBuilder ansMaker = new StringBuilder();
+        for(int idx = 0; idx < n; idx++) {
+            ansMaker.append(numbers[idx]).append("\n");
+        }
+        System.out.print(ansMaker);
+    }
 }

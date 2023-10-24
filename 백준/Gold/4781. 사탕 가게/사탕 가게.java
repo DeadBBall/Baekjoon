04dccd6f79cbccfc9e0c6125b929ff274e342356
morenmore <89844277+DeadBBall@users.noreply.github.com>
2023-10-24 19:52:52 +0900

    [Gold IV] Title: 사탕 가게, Time: 5976 ms, Memory: 167716 KB -BaekjoonHub

---
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static int n, m, ans;
+    static Candy[] candies;
+    static int[] dp;
+    static StringBuilder ansMaker = new StringBuilder();
+
+    public static void main(String[] args) {
+        while(true) {
+            input();
+            if(n == 0 && m == 0) break;
+            calculateHighCalorie();
+        }
+        System.out.println(ansMaker);
+    }
+
+    static void input() {
+        n = sc.nextInt();
+        m = (int)(sc.nextFloat() * 100.0 + 0.5);
+        if(n == 0 && m == 0) return;
+
+        dp = new int[m + 1];
+        candies = new Candy[n];
+        ans = 0;
+
+        for(int candyIdx = 0; candyIdx < n; candyIdx++) {
+            int calorie = sc.nextInt();
+            int cost = (int)(sc.nextFloat() * 100.0 + 0.5);
+            candies[candyIdx] = new Candy(calorie, cost);
+        }
+
+        Arrays.sort(candies, new Comparator<Candy>() {
+            @Override
+            public int compare(Candy o1, Candy o2) {
+                return o1.cost - o2.cost;
+            }
+        });
+    }
+
+    static void calculateHighCalorie() {
+        for(int money = candies[0].cost; money <= m; money++) {
+            for(int candyIdx = 0; candyIdx < n; candyIdx++) {
+                if(money < candies[candyIdx].cost) continue;
+
+                dp[money] = Math.max(dp[money], dp[money - candies[candyIdx].cost] + candies[candyIdx].calorie);
+                ans = Math.max(dp[money], ans);
+            }
+        }
+        ansMaker.append(ans).append("\n");
+    }
+
+    static class Candy {
+        int calorie;
+        int cost;
+
+        public Candy(int calorie, int cost) {
+            this.calorie = calorie;
+            this.cost = cost;
+        }
+    }
+}

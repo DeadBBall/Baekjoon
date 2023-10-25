108d6a9aae2c877c268d48d62250e6432d7c522b
morenmore <89844277+DeadBBall@users.noreply.github.com>
2023-10-25 09:07:58 +0900

    [Silver III] Title: 컴포트, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Silver/3258. 컴포트/컴포트.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static int n, z, m, k;
+    static boolean[] obstacles;
+    static boolean[] visit;
+
+    public static void main(String[] args) {
+        input();
+        searchMinMove();
+        System.out.println(k);
+    }
+
+    static void input() {
+        n = sc.nextInt();
+        z = sc.nextInt();
+        m = sc.nextInt();
+        obstacles = new boolean[n + 1];
+        visit = new boolean[n + 1];
+
+        for(int idx = 0; idx < m; idx++) {
+            int pos = sc.nextInt();
+            obstacles[pos] = true;
+        }
+        k = 1;
+    }
+
+    static void searchMinMove() {
+        if(m == 0) return;
+
+        for(int move = 1; ;move++) {
+            int now = 1;
+            Arrays.fill(visit, false);
+            visit[now] = true;
+            while(true) {
+                now = (now + move) % n;
+                if(now == 0) now = n;
+                if(now == z) {
+                    k = move;
+                    return;
+                }
+                if(obstacles[now] || visit[now]) break;
+                visit[now] = true;
+            }
+        }
+    }
+}

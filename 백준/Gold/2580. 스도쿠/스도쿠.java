7944b5b943680e8463c35f04260de987c5389584
morenmore <89844277+DeadBBall@users.noreply.github.com>
2023-12-10 16:42:01 +0900

    [Gold IV] Title: 스도쿠, Time: 2940 ms, Memory: 305160 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -0,0 +1,91 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+	static Scanner sc = new Scanner(System.in);
+	static int[][] board;
+	static StringBuilder ansMaker;
+	static Set<Integer> numberSet;
+	static boolean getAnswer;
+	
+	public static void main(String[] args) {
+		input();
+		playSudoku(0, 0);
+	}
+	
+	static void input() {
+		board = new int[9][9];
+		ansMaker = new StringBuilder();
+		numberSet = new HashSet<>();
+		
+		for(int y = 0; y < 9; y++) {
+			for(int x = 0; x < 9; x++) {
+				board[y][x] = sc.nextInt();
+			}
+		}
+	}
+	
+	static void playSudoku(int nowY, int nowX) {
+		if(nowY == 9 && nowX == 0) {
+			for(int y = 0; y < 9; y++) {
+				for(int x = 0; x < 9; x++) {
+					ansMaker.append(board[y][x] + " ");
+				}
+				ansMaker.append("\n");
+			}
+			System.out.print(ansMaker);
+			getAnswer = true;
+			return;
+		}
+		
+		int nextX = (nowX + 1) % 9;
+		int nextY = nextX == 0 ? nowY + 1: nowY;
+		
+		if(board[nowY][nowX] == 0) {
+			for(int num = 1; num <= 9; num++) {
+				if(getAnswer) return;
+				board[nowY][nowX] = num;
+		
+				if(isPossible(nowY, nowX))
+					playSudoku(nextY, nextX);
+				
+				board[nowY][nowX] = 0;
+			}
+		}
+		
+		else
+			playSudoku(nextY, nextX);
+	}
+	
+	static boolean isPossible(int nowY, int nowX) {
+		numberSet.clear();
+		for(int y = 0; y < 9; y++) {
+			int nowNumber = board[y][nowX];
+			if(nowNumber == 0) continue;
+			if(numberSet.contains(nowNumber)) return false;
+			numberSet.add(nowNumber);
+		}
+		
+		numberSet.clear();
+		for(int x = 0; x < 9; x++) {
+			int nowNumber = board[nowY][x];
+			if(nowNumber == 0) continue;
+			if(numberSet.contains(nowNumber)) return false;
+			numberSet.add(nowNumber);
+		}
+		
+		numberSet.clear();
+		for(int y = 0; y < 3; y++) {
+			for(int x = 0; x < 3; x++) {
+				int nowNumber = board[nowY / 3 * 3 + y][nowX / 3 * 3 + x];
+				if(nowNumber == 0) continue;
+				if(numberSet.contains(nowNumber)) return false;
+				numberSet.add(nowNumber);
+			}
+		}
+		
+		return true;
+	}
+	
+}

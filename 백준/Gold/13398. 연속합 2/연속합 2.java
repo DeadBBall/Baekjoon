04e3b49db1f48f448fb8523e889d97c00c72169e
morenmore <89844277+DeadBBall@users.noreply.github.com>
2024-03-12 15:24:22 +0900

    [Gold V] Title: 연속합 2, Time: 828 ms, Memory: 111448 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.java
+++ 백준/Gold/13398. 연속합 2/연속합 2.java
@@ -4,7 +4,7 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
     static int n, ans;
     static int[] numbers;
-    static int[] dpLeft, dpRight;
+    static int[][] dp;
 
     public static void main(String[] args) {
         input();
@@ -15,29 +15,22 @@ public class Main {
     static void input() {
         n = sc.nextInt();
         numbers = new int[n];
-        dpLeft = new int[n];
-        dpRight = new int[n];
+        dp = new int[n][2];
 
         for(int numIdx = 0; numIdx < n; numIdx++) {
             numbers[numIdx] = sc.nextInt();
         }
 
-        dpLeft[0] = ans = numbers[0];
-        dpRight[n - 1] = numbers[n - 1];
+        ans = dp[0][0] = dp[0][1] = numbers[0];
     }
 
     static void findSequence() {
         for(int numIdx = 1; numIdx < n; numIdx++) {
-            dpLeft[numIdx] = Math.max(dpLeft[numIdx - 1] + numbers[numIdx] , numbers[numIdx]);
-            dpRight[n - 1 - numIdx] = Math.max(dpRight[n - numIdx] + numbers[n - 1 - numIdx], numbers[n - 1 - numIdx]);
-            
-            ans = Math.max(ans, dpLeft[numIdx]);
-        }
+            dp[numIdx][0] = Math.max(dp[numIdx - 1][0] + numbers[numIdx], numbers[numIdx]);
 
-        for(int numIdx = 1; numIdx < n - 1; numIdx++) {
-            int sum = dpLeft[numIdx - 1] + dpRight[numIdx + 1];
+            dp[numIdx][1] = Math.max(dp[numIdx - 1][1] + numbers[numIdx], dp[numIdx - 1][0]);
 
-            ans = Math.max(sum, ans);
+            ans = Math.max(ans, Math.max(dp[numIdx][0], dp[numIdx][1]));
         }
     }
 

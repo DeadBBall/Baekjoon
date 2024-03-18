54dc99fdcca75486bd3fdb1c9475c997bec12afa
morenmore <89844277+DeadBBall@users.noreply.github.com>
2024-03-18 15:06:25 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 276 ms, Memory: 19908 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
@@ -1,69 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    static final int EMPTY = 0;
-    static final int RIGHT = 2;
-    static final int DIAGONAL = 3;
-    static final int DOWN = 4;
-    static final int[] DY = {0, 1, 1};
-    static final int[] DX = {1, 1, 0};
+    static final int WALL = 1;
+    static final int RIGHT = 0;
+    static final int DIAGONAL = 1;
+    static final int DOWN = 2;
     static Scanner sc = new Scanner(System.in);
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int n;
     static int[][] board;
     static long[][][] dp;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         input();
-        System.out.println(movePipe(0, 1, RIGHT));
+        movePipe();
+        System.out.println(dp[n - 1][n - 1][RIGHT] + dp[n - 1][n - 1][DIAGONAL] + dp[n - 1][n - 1][DOWN]);
     }
 
-    static void input() throws IOException {
-//        n = Integer.parseInt(br.readLine());
+    static void input() {
         n = sc.nextInt();
         dp = new long[n][n][3];
         board = new int[n][n];
 
         for(int y = 0; y < n; y++) {
-//            StringTokenizer st = new StringTokenizer(br.readLine());
             for(int x = 0; x < n; x++) {
-//                board[y][x] = Integer.parseInt(st.nextToken());
                 board[y][x] = sc.nextInt();
             }
         }
     }
 
-    static long movePipe(int y, int x, int drct) {
-        if(y == n - 1 && x == n - 1) return 1;
+    static void movePipe() {
+        dp[0][1][RIGHT] = 1;
 
-        if(dp[y][x][drct - 2] != 0) return dp[y][x][drct - 2];
+        for(int y = 0; y < n; y++) {
+            for(int x = 2; x < n; x++) {
+                if(board[y][x] == WALL) continue;
 
-        for(int dir = 0; dir < 3; dir++) {
-            if(dir + 2 == RIGHT && drct == DOWN) continue;
-            if(dir + 2 == DOWN && drct == RIGHT) continue;
+                dp[y][x][RIGHT] = dp[y][x - 1][RIGHT]+dp[y][x - 1][DIAGONAL];
 
-            int ny = y + DY[dir];
-            int nx = x + DX[dir];
+                if(y == 0) continue;
 
-            if(!canMove(ny, nx)) continue;
+                dp[y][x][DOWN] = dp[y - 1][x][DOWN] + dp[y - 1][x][DIAGONAL];
 
-            if(dir + 2 == DIAGONAL && !canMoveDiagonal(ny, nx)) continue;
+                if(board[y - 1][x] == WALL || board[y][x - 1] == WALL) continue;
 
-            dp[y][x][drct - 2] += movePipe(ny, nx, dir + 2);
+                dp[y][x][DIAGONAL] = dp[y - 1][x - 1][RIGHT] + dp[y - 1][x - 1][DIAGONAL] + dp[y - 1][x - 1][DOWN];
+            }
         }
-
-        return dp[y][x][drct - 2];
     }
-
-    static boolean canMove(int y, int x) {
-        return x >= 0 && x < n && y >= 0 && y < n && board[y][x] == EMPTY;
-    }
-
-    static boolean canMoveDiagonal(int y, int x) {
-        return canMove(y, x) && board[y - 1][x] == EMPTY && board[y][x - 1] == EMPTY;
-    }
-
 }

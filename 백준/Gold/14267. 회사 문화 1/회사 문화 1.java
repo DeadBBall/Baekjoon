33e02353075c8b48bad66b73cd5ef2e360ea9ea7
morenmore <89844277+DeadBBall@users.noreply.github.com>
2024-04-10 13:09:46 +0900

    [Gold IV] Title: 회사 문화 1, Time: 2324 ms, Memory: 267600 KB -BaekjoonHub

---
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static List<Integer>[] subordinates;
+    static int n, m;
+    static int[] dp;
+
+    public static void main(String[] args) {
+        input();
+        praiseSubordinates(1);
+        for(int staff = 1; staff <= n; staff++) {
+            System.out.print(dp[staff] + " ");
+        }
+    }
+
+    static void input() {
+        n = sc.nextInt();
+        m = sc.nextInt();
+        dp = new int[n + 1];
+        subordinates = new List[n + 1];
+
+        for(int staffIdx = 1; staffIdx <= n; staffIdx++) {
+            subordinates[staffIdx] = new ArrayList<>();
+        }
+
+        for(int staffIdx = 1; staffIdx <= n; staffIdx++) {
+            int boss = sc.nextInt();
+
+            if(boss == -1) continue;
+
+            subordinates[boss].add(staffIdx);
+        }
+
+        for(int praiseIdx = 0; praiseIdx < m; praiseIdx++) {
+            int staff = sc.nextInt();
+            int power = sc.nextInt();
+
+            dp[staff] += power;
+        }
+    }
+
+    static void praiseSubordinates(int now) {
+        for(int subordinate : subordinates[now]) {
+            dp[subordinate] += dp[now];
+            praiseSubordinates(subordinate);
+        }
+    }
+
+}

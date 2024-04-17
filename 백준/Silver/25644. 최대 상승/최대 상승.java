6bf79110a33094316bd0ac1327192503e80a8ee5
morenmore <89844277+DeadBBall@users.noreply.github.com>
2024-04-17 12:30:23 +0900

    [Silver V] Title: 최대 상승, Time: 1136 ms, Memory: 180664 KB -BaekjoonHub

--- 백준/Silver/25644. 최대 상승/최대 상승.java
+++ 백준/Silver/25644. 최대 상승/최대 상승.java
@@ -2,8 +2,7 @@ import java.util.*;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
-    static int n, ans, min, max;
-    static int[] costs;
+    static int n, ans;
 
     public static void main(String[] args) {
         input();
@@ -13,25 +12,16 @@ public class Main {
 
     static void input() {
         n = sc.nextInt();
-        costs = new int[n];
-
-        for(int day = 0; day < n; day++) {
-            costs[day] = sc.nextInt();
-        }
     }
 
     static void makeMoney() {
-        min = max = costs[0];
-
-        for(int day = 1; day < n; day++) {
-            if(costs[day] < min) {
-                min = costs[day];
-                max = costs[day];
-            }
-            else if(costs[day] > max) {
-                max = costs[day];
-                ans = Math.max(ans, max - min);
-            }
+        int min = Integer.MAX_VALUE;
+        
+        for(int day = 0; day < n; day++) {
+            int cost = sc.nextInt();
+            
+            min = Math.min(min, cost);
+            ans = Math.max(cost - min, ans);
         }
     }
 

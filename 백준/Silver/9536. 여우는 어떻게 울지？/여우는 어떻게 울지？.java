d59a83b1a4dfd7fe66044c05b7292357cea65721
morenmore <89844277+DeadBBall@users.noreply.github.com>
2024-04-27 10:44:55 +0900

    [Silver III] Title: 여우는 어떻게 울지?, Time: 232 ms, Memory: 17996 KB -BaekjoonHub

--- 백준/Silver/9536. 여우는 어떻게 울지？/여우는 어떻게 울지？.java
+++ 백준/Silver/9536. 여우는 어떻게 울지？/여우는 어떻게 울지？.java
@@ -4,7 +4,7 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
     static StringBuilder ansMaker = new StringBuilder();
     static String[] voices;
-    static Map<String, String> animals;
+    static Set<String> animals;
 
     public static void main(String[] args) {
         int t = Integer.parseInt(sc.nextLine());
@@ -20,20 +20,20 @@ public class Main {
     static void input() {
         voices = sc.nextLine().split(" ");
 
-        animals = new HashMap<>();
+        animals = new HashSet<>();
 
         while(true) {
             String[] inputStr = sc.nextLine().split(" ");
 
             if(inputStr[0].equals("what")) break;
 
-            animals.put(inputStr[2], inputStr[0]);
+            animals.add(inputStr[2]);
         }
     }
 
     static void findFox() {
         for(String voice : voices) {
-            if(!animals.containsKey(voice)) {
+            if(!animals.contains(voice)) {
                 ansMaker.append(voice).append(" ");
             }
         }

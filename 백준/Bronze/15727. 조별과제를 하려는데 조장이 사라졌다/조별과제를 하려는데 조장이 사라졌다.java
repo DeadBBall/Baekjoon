c8b87db670c466ee0ddea95b4130015713edc760
morenmore <89844277+DeadBBall@users.noreply.github.com>
2025-01-21 18:06:47 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 172 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println((sc.nextInt() + 4) / 5) ;
+
+    }
+
+}

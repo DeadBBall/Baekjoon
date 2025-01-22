76555e90801b2f01cd3664959452acf513fbb257
morenmore <89844277+DeadBBall@users.noreply.github.com>
2025-01-23 06:14:41 +0900

    [Bronze V] Title: 제리와 톰, Time: 196 ms, Memory: 18488 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int p = sc.nextInt();
+        int q = sc.nextInt();
+
+        System.out.println(q - p + " " + q);
+    }
+
+}
